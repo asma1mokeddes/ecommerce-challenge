bed40ddbb313c9027ed545e38b6079db05959ac4
asma1mokeddes <asma1>
2023-10-13 11:05:56 +0200

    Create ci-cd.yml

---
+++ tests/unit/register.test.js
@@ -0,0 +1,15 @@
+// tests/unit/authRoutes.test.js
+
+describe("register function", () => {
+    it("should register a new user with valid data", () => {
+        // Votre test ici
+    });
+
+    it("should handle missing data gracefully", () => {
+        // Votre test ici
+    });
+
+    it("should handle duplicate email address", () => {
+        // Votre test ici
+    });
+});
