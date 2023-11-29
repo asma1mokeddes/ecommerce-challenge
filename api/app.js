f330cc244a0f06627381f22a51e755a7c9b16eb5
Hicham AISSAOUI <85109518+hicham-2@users.noreply.github.com>
2023-11-29 21:42:52 +0100

    Update app.js

--- api/app.js
+++ api/app.js
@@ -7,7 +7,6 @@ import categories from "./src/router/categoriesRouter.js";
 import promos from "./src/router/promosRouter.js";
 import brands from "./src/router/brandsRouter.js";
 import emails from "./src/router/emailsRouter.js";
-import shipments from "./src/router/shipmentsRoutes.js";
 
 import dotenv from "dotenv";
 import cors from "cors";
@@ -51,7 +50,6 @@ app.use("/categories", categories);
 app.use("/promos", promos);
 app.use("/brands", brands);
 app.use("/emails", emails);
-app.use("/shipments", shipments);
 
 try {
     console.log("process.env.DB_URI ====", process.env.DB_URI);
