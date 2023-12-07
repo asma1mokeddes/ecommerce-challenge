f67ec76b7a3aa185ba4d747f9fb1422f904dd4d6
Asmaa <amokeddes@myges.fr>
2023-12-07 10:23:28 +0100

    Tests unit

---
+++ api/tests/unit/promo.test.js
@@ -0,0 +1,299 @@
+// Imports
+const {
+    getPromos,
+    createPromo,
+    updatePromo,
+    deletePromo,
+} = require("../../src/routes/promosRoutes.js");
+const PromoMongo = require("../../src/models/promo.js");
+const Promo = require("../../src/models/promo.model.js");
+
+// Mock dependencies
+jest.mock("../../src/models/promo.js", () => ({
+    find: jest.fn(),
+    findOne: jest.fn(),
+    findOneAndUpdate: jest.fn(),
+    create: jest.fn(),
+    findOneAndDelete: jest.fn(),
+}));
+
+jest.mock("../../src/models/promo.model.js", () => ({
+    findOne: jest.fn(),
+    update: jest.fn(),
+    create: jest.fn(),
+    destroy: jest.fn(),
+}));
+
+describe("Promo Routes", () => {
+    beforeEach(() => {
+        // Reset all mocks before each test
+        jest.clearAllMocks();
+    });
+
+    // Test for getPromos
+    describe("getPromos", () => {
+        it("should retrieve all promos", async () => {
+            PromoMongo.find.mockResolvedValue([
+                { promoCode: "PROMO1", expirationDate: new Date() },
+            ]);
+            const req = {};
+            const res = { json: jest.fn(), status: jest.fn().mockReturnThis() };
+            await getPromos(req, res);
+            expect(res.json).toHaveBeenCalledWith(expect.any(Array));
+        });
+        // Test for no promotions available
+        it("should handle no promotions available", async () => {
+            PromoMongo.find.mockResolvedValue([]); // Mock an empty array return
+            const req = {};
+            const res = { json: jest.fn(), status: jest.fn().mockReturnThis() };
+
+            await getPromos(req, res);
+
+            expect(res.json).toHaveBeenCalledWith([]); // Expect an empty array response
+            expect(res.status).not.toHaveBeenCalledWith(500); // Ensure no server error
+        });
+
+        // Test for handling server errors
+        it("should handle server errors gracefully", async () => {
+            PromoMongo.find.mockRejectedValue(
+                new Error("Database connection error")
+            ); // Mock a server error
+            const req = {};
+            const res = { json: jest.fn(), status: jest.fn().mockReturnThis() };
+
+            await getPromos(req, res);
+
+            expect(res.status).toHaveBeenCalledWith(500);
+            expect(res.json).toHaveBeenCalledWith(
+                expect.objectContaining({
+                    message: expect.stringContaining(
+                        "Une erreur est survenue lors de la récupération des promotions."
+                    ),
+                })
+            );
+        });
+    });
+
+    // Test for createPromo
+    describe("createPromo", () => {
+        it("should create a new promo with valid data", async () => {
+            const req = {
+                body: {
+                    promoCode: "NEWPROMO",
+                    expirationDate: new Date(),
+                },
+            };
+            const res = { json: jest.fn(), status: jest.fn().mockReturnThis() };
+
+            Promo.findOne.mockResolvedValue(null);
+            PromoMongo.findOne.mockResolvedValue(null);
+
+            await createPromo(req, res);
+
+            expect(res.status).toHaveBeenCalledWith(201);
+            expect(res.json).toHaveBeenCalledWith(
+                expect.objectContaining({
+                    message: "Code promo créé avec succès",
+                    promoCode: req.body.promoCode,
+                })
+            );
+        });
+        // Test for missing required fields
+        it("should not create a promo if required fields are missing", async () => {
+            const req = {
+                body: {
+                    // Missing promoCode or expirationDate
+                },
+            };
+            const res = { json: jest.fn(), status: jest.fn().mockReturnThis() };
+
+            await createPromo(req, res);
+
+            expect(res.status).toHaveBeenCalledWith(400);
+            expect(res.json).toHaveBeenCalledWith(
+                expect.objectContaining({
+                    message:
+                        "Champs requis manquants. Veuillez fournir le code ainsi que la date d'expiration de la promotion.",
+                })
+            );
+        });
+
+        // Test for duplicate promo code
+        it("should not create a promo if the code already exists", async () => {
+            const req = {
+                body: {
+                    promoCode: "ASMA10",
+                    expirationDate: new Date(),
+                },
+            };
+            const res = { json: jest.fn(), status: jest.fn().mockReturnThis() };
+
+            // Simulate finding an existing promo
+            Promo.findOne.mockResolvedValue({ promoCode: "ASMA10" });
+            PromoMongo.findOne.mockResolvedValue({ promoCode: "ASMA10" });
+
+            await createPromo(req, res);
+
+            expect(res.status).toHaveBeenCalledWith(409);
+            expect(res.json).toHaveBeenCalledWith(
+                expect.objectContaining({
+                    error: "Ce code promo existe déjà",
+                })
+            );
+        });
+    });
+
+    describe("updatePromo", () => {
+        // Test for successful promo update
+        it("should update a promo successfully", async () => {
+            const promoId = 1; // Example promo ID
+            const req = {
+                params: { promoId },
+                body: {
+                    promoCode: "UPDATEDPROMO",
+                    expirationDate: new Date(),
+                },
+            };
+            const res = { json: jest.fn(), status: jest.fn().mockReturnThis() };
+
+            // Mock successful update scenarios
+            Promo.findOne.mockResolvedValue(null);
+            Promo.update.mockResolvedValue([
+                1,
+                [{ promoCode: "UPDATEDPROMO" }],
+            ]);
+            PromoMongo.findOneAndUpdate.mockResolvedValue({
+                promoCode: "UPDATEDPROMO",
+                expirationDate: new Date(),
+            });
+
+            await updatePromo(req, res);
+
+            expect(res.json).toHaveBeenCalledWith(
+                expect.objectContaining({
+                    message: "Le code promo a bien été mise à jour.",
+                })
+            );
+        });
+
+        // Test for promo not found
+        it("should not update a non-existent promo", async () => {
+            const promoId = 2; // Non-existent promo ID
+            const req = {
+                params: { promoId },
+                body: {
+                    promoCode: "NONEXISTENTPROMO",
+                    expirationDate: new Date(),
+                },
+            };
+            const res = { json: jest.fn(), status: jest.fn().mockReturnThis() };
+
+            // Mock promo not found scenario
+            Promo.findOne.mockResolvedValue(null);
+            Promo.update.mockResolvedValue([0, []]);
+            PromoMongo.findOneAndUpdate.mockResolvedValue(null);
+
+            await updatePromo(req, res);
+
+            expect(res.status).toHaveBeenCalledWith(404);
+            expect(res.json).toHaveBeenCalledWith(
+                expect.objectContaining({
+                    message: `Le code promo avec l'ID ${promoId} n'existe pas.`,
+                })
+            );
+        });
+
+        // Test for updating a promo with a duplicate code
+        it("should not update a promo with a duplicate code", async () => {
+            const promoId = 3; // Example promo ID
+            const req = {
+                params: { promoId },
+                body: {
+                    promoCode: "DUPLICATEPROMO",
+                    expirationDate: new Date(),
+                },
+            };
+            const res = { json: jest.fn(), status: jest.fn().mockReturnThis() };
+
+            // Mock finding a duplicate promo
+            Promo.findOne.mockResolvedValue({ promoCode: "DUPLICATEPROMO" });
+
+            await updatePromo(req, res);
+
+            expect(res.status).toHaveBeenCalledWith(409);
+            expect(res.json).toHaveBeenCalledWith(
+                expect.objectContaining({
+                    message: "Ce code promo existe déjà.",
+                })
+            );
+        });
+    });
+
+    describe("deletePromo", () => {
+        // Test for successful promo deletion
+        it("should delete a promo successfully", async () => {
+            const promoId = 1; // Example promo ID
+            const req = {
+                params: { promoId },
+            };
+            const res = { json: jest.fn(), status: jest.fn().mockReturnThis() };
+
+            // Mock successful deletion scenarios
+            Promo.findOne.mockResolvedValue({ id: promoId });
+            Promo.destroy.mockResolvedValue(1);
+            PromoMongo.findOneAndDelete.mockResolvedValue({ promoId });
+
+            await deletePromo(req, res);
+
+            expect(res.json).toHaveBeenCalledWith(
+                expect.objectContaining({
+                    message: "Le code promo a bien été supprimé !",
+                })
+            );
+        });
+
+        // Test for promo not found
+        it("should not delete a non-existent promo", async () => {
+            const promoId = 2; // Non-existent promo ID
+            const req = {
+                params: { promoId },
+            };
+            const res = { json: jest.fn(), status: jest.fn().mockReturnThis() };
+
+            // Mock promo not found scenario
+            Promo.findOne.mockResolvedValue(null); // Promo not found in PostgreSQL
+
+            await deletePromo(req, res);
+
+            expect(res.status).toHaveBeenCalledWith(404);
+            expect(res.json).toHaveBeenCalledWith(
+                expect.objectContaining({
+                    message: `Impossible de supprimer le code promo avec id=${promoId}.`,
+                })
+            );
+        });
+
+        // Test for handling server errors
+        it("should handle server errors during deletion", async () => {
+            const promoId = 3; // Example
+            const req = {
+                params: { promoId },
+            };
+            const res = { json: jest.fn(), status: jest.fn().mockReturnThis() };
+
+            // Mock a server error scenario
+            Promo.findOne.mockRejectedValue(
+                new Error("Database connection error")
+            );
+
+            await deletePromo(req, res);
+
+            expect(res.status).toHaveBeenCalledWith(500);
+            expect(res.json).toHaveBeenCalledWith(
+                expect.objectContaining({
+                    message: `Erreur lors de la suppression de ce code promo`,
+                })
+            );
+        });
+    });
+});
---
+++ api/tests/unit/register.test.js
@@ -0,0 +1,181 @@
+// Import the function to test
+const { register } = require("../../src/routes/authRoutes.js");
+const UserMongo = require("../../src/models/user.js");
+const User = require("../../src/models/user.model.js");
+const { sendActivationEmail } = require("../../src/routes/emailsRoutes.js");
+
+// Mock dependencies
+jest.mock("../../src/models/user.js", () => ({
+    findOne: jest.fn().mockResolvedValue(null), // Mock no existing UserMongo user
+    create: jest.fn().mockResolvedValue({ id: 1 }), // Mock UserMongo model creation
+}));
+
+jest.mock("../../src/models/user.model.js", () => ({
+    findOne: jest.fn().mockResolvedValue(null), // Mock no existing User user
+    create: jest.fn().mockResolvedValue({ id: 1 }), // Mock User model creation
+}));
+
+jest.mock("../../src/routes/emailsRoutes.js", () => ({
+    sendActivationEmail: jest.fn().mockResolvedValue(), // Mock email sending
+}));
+
+describe("register function", () => {
+    beforeEach(() => {
+        // Reset all mocks before each test
+        jest.clearAllMocks();
+    });
+
+    it("should register a new user with valid data", async () => {
+        // Mock request object with valid data
+        const req = {
+            body: {
+                firstName: "John",
+                lastName: "Doe",
+                emailAddress: "john@example.com",
+                dateOfBirth: "1990-01-01",
+                password: "securePassword123",
+            },
+        };
+
+        // Mock response object
+        const res = {
+            json: jest.fn(),
+            status: jest.fn().mockReturnThis(),
+        };
+
+        // Execute the register function
+        await register(req, res);
+
+        // Expectations and Assertions
+        expect(UserMongo.findOne).toHaveBeenCalledWith({
+            emailAddress: "john@example.com",
+        });
+        expect(User.findOne).toHaveBeenCalledWith({
+            where: { emailAddress: "john@example.com" },
+        });
+        expect(UserMongo.create).toHaveBeenCalledWith({
+            firstName: "John",
+            lastName: "Doe",
+            emailAddress: "john@example.com",
+            dateOfBirth: "1990-01-01",
+            role: "ROLE_USER",
+        });
+        expect(User.create).toHaveBeenCalledWith({
+            firstName: "John",
+            lastName: "Doe",
+            emailAddress: "john@example.com",
+            dateOfBirth: "1990-01-01",
+            password: expect.any(String), // since the password would be hashed
+            passwordModificationDate: expect.any(Date),
+            role: "ROLE_USER",
+        });
+        expect(sendActivationEmail).toHaveBeenCalledWith(
+            req,
+            res,
+            expect.any(String)
+        );
+        expect(res.json).toHaveBeenCalledWith({
+            message: "Utilisateur créé avec succès",
+            token: expect.any(String),
+        });
+    });
+
+    it("should handle missing data gracefully", async () => {
+        // Mock request object with missing data
+        const req = {
+            body: {
+                // Intentionally leaving out some required fields
+                firstName: "John",
+                lastName: "Doe",
+                // emailAddress and password are missing
+            },
+        };
+
+        // Mock response object
+        const res = {
+            json: jest.fn(),
+            status: jest.fn().mockReturnThis(),
+        };
+
+        // Execute the register function
+        await register(req, res);
+
+        // Check if the status method was called with 500 (Internal Server Error)
+        expect(res.status).toHaveBeenCalledWith(500);
+        // Check if the json method was called with an error message
+        expect(res.json).toHaveBeenCalledWith(
+            expect.objectContaining({
+                error: expect.stringContaining("Invalid arguments"),
+            })
+        );
+    });
+
+    it("should handle duplicate email address", async () => {
+        // Mock request object with a duplicate email address
+        const req = {
+            body: {
+                firstName: "Asma",
+                lastName: "Mokeddes",
+                emailAddress: "mokeddes.asma@yopmail.com",
+                password: "Asma17930303@ye!@h",
+                dateOfBirth: "2000-10-07",
+            },
+        };
+
+        // Mock response object
+        const res = {
+            json: jest.fn(),
+            status: jest.fn().mockReturnThis(),
+        };
+
+        // Mock findOne to simulate existing user
+        UserMongo.findOne.mockResolvedValue({
+            emailAddress: "john@example.com",
+        });
+
+        // Execute the register function
+        await register(req, res);
+
+        // Check if the status method was called with 500 (Internal Server Error)
+        expect(res.status).toHaveBeenCalledWith(500);
+        // Check if the json method was called with an error message indicating duplicate email
+        expect(res.json).toHaveBeenCalledWith(
+            expect.objectContaining({
+                error: expect.stringContaining("déjà utilisée"), // Part of the error message in your code
+            })
+        );
+    });
+
+    it("should handle insufficient password strength", async () => {
+        // Mock request object with a weak password
+        const req = {
+            body: {
+                firstName: "Alice",
+                lastName: "Smith",
+                emailAddress: "alice@example.com",
+                dateOfBirth: "1990-03-01",
+                password: "123", // Intentionally weak password
+            },
+        };
+
+        // Mock response object
+        const res = {
+            json: jest.fn(),
+            status: jest.fn().mockReturnThis(),
+        };
+
+        // Execute the register function
+        await register(req, res);
+
+        // Check if the status method was called with 400 (Bad Request) or similar
+        expect(res.status).toHaveBeenCalledWith(expect.any(Number));
+        // Check if the json method was called with an error message indicating weak password
+        expect(res.json).toHaveBeenCalledWith(
+            expect.objectContaining({
+                error: expect.stringContaining(
+                    "Password does not meet complexity requirements"
+                ),
+            })
+        );
+    });
+});
